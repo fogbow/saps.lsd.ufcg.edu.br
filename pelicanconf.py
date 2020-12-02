8a0ee1d263968a4edc680b11e02d0f49b0f1e94d
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-12-02 14:06:05 -0300

    Refactored code according to Google Python Style

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,14 +23,18 @@ THEME = 'themes'
 DIRECT_TEMPLATES = ['index']
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('You can modify those links in your config file', '#'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('You can add links in your config file', '#'),
+    ('Another social link', '#'),
+)
 
 DEFAULT_PAGINATION = False
 
